3a97473891800ac744621bc56f015938a60e7309
Kiffin Gish <kiffin.gish@planet.nl>
2019-01-13 21:50:55 +0100

    Readme stuff.

--- 01-init.rb
+++ 01-init.rb
@@ -17,27 +17,53 @@ unless RUBY_VERSION == version
 end
 
 # Load environment
-@debug = ENV['DEBUG'] == 'true'
-@data_dir = ENV['DATA_DIR'] || 'data'
-@converter = ENV['CONVERTER'] || 'markdown2confluence'
+DEBUG = ENV['DEBUG'] == 'true'
+DATA = ENV['DATA'] || 'data'
+CONVERTER = ENV['CONVERTER'] || 'markdown2confluence'
+API = ENV['API'] || throw('API must be defined')
+SPACE = ENV['SPACE'] || throw('SPACE must be defined')
+USER = ENV['USER'] || throw('USER must be defined')
+EMAIL = ENV['EMAIL'] || throw('EMAIL must be defined')
+PASSWORD = ENV['PASSWORD'] || throw('PASSWORD must be defined')
 
 # Display environment
-puts "\n--- ENVIRONMENT ---"
-puts "DEBUG:     '#{@debug}'"
-puts "DATA_DIR:  '#{@data_dir}'"
-puts "CONVERTER: '#{@converter}'"
-puts "-------------------"
+puts "DEBUG:    : '#{DEBUG}'"
+puts "DATA      : '#{DATA}'"
+puts "CONVERTER : '#{CONVERTER}'"
+puts "API       : '#{API}'"
+puts "SPACE     : '#{SPACE}'"
+puts "USER      : '#{USER}'"
+
+HEADERS = {
+    'Authorization': "Basic #{Base64.encode64("#{EMAIL}:#{PASSWORD}")}",
+    'Content-Type': 'application/json; charset=utf-8',
+    'Accept': 'application/json'
+}.freeze
+
+def get_spaces
+  url = "#{API}/spaces"
+  begin
+    response = RestClient::Request.execute(method: :get, url: url, headers: HEADERS)
+    puts response.inspect
+    puts "GET #{url} => OK"
+  rescue => e
+    puts "GET #{url} => NOK (#{e.message})"
+  end
+end
+
+get_spaces
+exit
 
 # List all files present in data directory
-files = Dir.children(@data_dir)
+files = Dir.children(DATA)
 puts "\nFILES: #{files.length}"
 files.each do |file|
   next unless /\.md$/.match(file)
-  infile = "#{@data_dir}/#{file}"
-  text = File.read(infile)
-  puts "#{file} => #{text.length}"
-  outfile = "#{@data_dir}/#{file.sub(/\.md$/, '')}.confluence"
-  output = `#{@converter} #{infile} >#{outfile} 2>&1`
+  infile = "#{DATA}/#{file}"
+  outfile = "#{DATA}/#{file.sub(/\.md$/, '')}.confluence"
+  output = `#{CONVERTER} #{infile} >#{outfile}`
+  puts output unless output.length === 0
   puts outfile
 end
 puts "\nDone!"
+
