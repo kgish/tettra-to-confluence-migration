bfd30746edfddee9d59209146049a4b7172c8c93
Kiffin Gish <kiffin.gish@planet.nl>
2019-02-07 22:57:34 +0100

    Restored links (deleted accidently).

---
+++ lib/common.rb
@@ -0,0 +1,107 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'csv'
+require 'fileutils'
+require 'dotenv/load'
+require 'rest-client'
+require 'base64'
+
+# Check that the correct ruby version is being used.
+version = File.read('.ruby-version').strip
+puts "Ruby version: #{RUBY_VERSION}"
+unless RUBY_VERSION == version
+  puts "Ruby version = '#{version}' is required, run the following command first:"
+  puts "rvm use #{version}"
+  exit
+end
+
+# Load environment
+DEBUG = ENV['DEBUG'] == 'true'
+DATA = ENV['DATA'] || 'data'
+IMAGES = ENV['IMAGES'] || 'images'
+CONVERTER = ENV['CONVERTER'] || 'markdown2confluence'
+EXT = ENV['EXT'] || 'confluence'
+API = ENV['CONFLUENCE_API'] || throw('CONFLUENCE_API must be defined')
+SPACE = ENV['CONFLUENCE_SPACE'] || throw('CONFLUENCE_SPACE must be defined')
+EMAIL = ENV['CONFLUENCE_EMAIL'] || throw('CONFLUENCE_EMAIL must be defined')
+PASSWORD = ENV['CONFLUENCE_PASSWORD'] || throw('CONFLUENCE_PASSWORD must be defined')
+LOGFILE = ENV['TETTRA_LOGFILE'] || throw('TETTRA_LOGFILE must be defined')
+COMPANY = ENV['TETTRA_COMPANY'] || throw('TETTRA_COMPANY must be defined')
+
+# Display environment
+puts
+puts "DEBUG:    : '#{DEBUG}'"
+puts "DATA      : '#{DATA}'"
+puts "IMAGES    : '#{IMAGES}'"
+puts "CONVERTER : '#{CONVERTER}'"
+puts "EXT       : '#{EXT}'"
+puts "API       : '#{API}'"
+puts "SPACE     : '#{SPACE}'"
+puts "PASSWORD  : '*******'"
+puts "LOGFILE   : '#{LOGFILE}'"
+puts "COMPANY   : '#{COMPANY}'"
+puts
+
+Dir.mkdir(DATA) unless File.exists?(DATA)
+Dir.mkdir(IMAGES) unless File.exists?(IMAGES)
+
+HEADERS = {
+  'Authorization': "Basic #{Base64.encode64("#{EMAIL}:#{PASSWORD}")}",
+  'Content-Type': 'application/json; charset=utf-8',
+  'Accept': 'application/json'
+}.freeze
+
+def csv_to_array(pathname)
+  csv = CSV::parse(File.open(pathname) {|f| f.read})
+  fields = csv.shift
+  fields = fields.map {|f| f.downcase.tr(' ', '_')}
+  csv.map {|record| Hash[*fields.zip(record).flatten]}
+end
+
+def write_csv_file(filename, results)
+  puts filename
+  CSV.open(filename, 'wb') do |csv|
+    # Scan whole file to collect all possible field names so that
+    # the list of columns is complete.
+    fields = []
+    results.each do |result|
+      result.keys.each do |field|
+        fields << field unless fields.include?(field)
+      end
+    end
+    csv << fields
+    results.each do |result|
+      row = []
+      fields.each do |field|
+        row.push(result[field])
+      end
+      csv << row
+    end
+  end
+end
+
+def rest_client_exception(e, method, url, payload = {})
+  message = 'Unknown error'
+  begin
+    err = JSON.parse(e.response)
+    if err['errors'] && !err['errors'].empty?
+      message = err['errors'].map {|k, v| "#{k}: #{v}"}.join(' | ')
+    elsif err['errorMessages'] && !err['errorMessages'].empty?
+      message = err['errorMessages'].join(' | ')
+    elsif err['error']
+      message = err['error']
+      if err['error_description']
+        message += ": #{err['error_description']}"
+      end
+    elsif err['status-code']
+      message = "Status code: #{err['status-code']}"
+    else
+      message = e.to_s
+    end
+  rescue
+    message = e.to_s
+  end
+  puts "#{method} #{url}#{payload.empty? ? '' : ' ' + payload.inspect} => NOK (#{message})"
+  message
+end
--- tettra-to-confluence.rb
+++ tettra-to-confluence.rb
@@ -1,57 +1,9 @@
 # frozen_string_literal: true
 
-require 'json'
-require 'csv'
-require 'fileutils'
-require 'dotenv/load'
-require 'rest-client'
-require 'base64'
-require 'date'
+load './lib/common.rb'
 
 @categories_tree = nil
 @offset_to_item = {}
-
-# Check that the correct ruby version is being used.
-version = File.read('.ruby-version').strip
-puts "Ruby version: #{RUBY_VERSION}"
-unless RUBY_VERSION == version
-  puts "Ruby version = '#{version}' is required, run the following command first:"
-  puts "rvm use #{version}"
-  exit
-end
-
-# Load environment
-DEBUG = ENV['DEBUG'] == 'true'
-DATA = ENV['DATA'] || 'data'
-IMAGES = ENV['IMAGES'] || 'images'
-CONVERTER = ENV['CONVERTER'] || 'markdown2confluence'
-EXT = ENV['EXT'] || 'confluence'
-API = ENV['CONFLUENCE_API'] || throw('CONFLUENCE_API must be defined')
-SPACE = ENV['CONFLUENCE_SPACE'] || throw('CONFLUENCE_SPACE must be defined')
-EMAIL = ENV['CONFLUENCE_EMAIL'] || throw('CONFLUENCE_EMAIL must be defined')
-PASSWORD = ENV['CONFLUENCE_PASSWORD'] || throw('CONFLUENCE_PASSWORD must be defined')
-LOGFILE = ENV['TETTRA_LOGFILE'] || throw('TETTRA_LOGFILE must be defined')
-COMPANY = ENV['TETTRA_COMPANY'] || throw('TETTRA_COMPANY must be defined')
-
-# Display environment
-puts
-puts "DEBUG:    : '#{DEBUG}'"
-puts "DATA      : '#{DATA}'"
-puts "IMAGES    : '#{IMAGES}'"
-puts "CONVERTER : '#{CONVERTER}'"
-puts "EXT       : '#{EXT}'"
-puts "API       : '#{API}'"
-puts "SPACE     : '#{SPACE}'"
-puts "PASSWORD  : '*******'"
-puts "LOGFILE   : '#{LOGFILE}'"
-puts "COMPANY   : '#{COMPANY}'"
-puts
-
-HEADERS = {
-  'Authorization': "Basic #{Base64.encode64("#{EMAIL}:#{PASSWORD}")}",
-  'Content-Type': 'application/json; charset=utf-8',
-  'Accept': 'application/json'
-}.freeze
 
 def show_categories(categories_tree)
   categories_tree.each do |c|
@@ -96,17 +48,17 @@ def sanity_check
   pages.each { |page| not_found << page unless found[page] }
   unless not_found.empty?
     puts "\nNot found #{not_found.length}:"
-    not_found.each { |page| puts "* #{page}" }
+    not_found.sort.each { |page| puts "* #{page}" }
   end
   unless duplicates.empty?
     puts "\nDuplicates #{duplicates.length}:"
     duplicates.each { |page| puts "* #{page}" }
   end
   if !not_found.empty? || !duplicates.empty?
-    puts "\nSanity check => NOK"
+    puts "\nSanity check => NOK\n"
     # exit
   else
-    puts "\nSanity check => OK"
+    puts "\nSanity check => OK\n"
   end
 end
 
@@ -229,28 +181,6 @@ def confluence_get_space(name)
   confluence_get_spaces.detect { |space| space['name'] == name }
 end
 
-def write_csv_file(filename, results)
-  puts filename
-  CSV.open(filename, 'wb') do |csv|
-    # Scan whole file to collect all possible field names so that
-    # the list of columns is complete.
-    fields = []
-    results.each do |result|
-      result.keys.each do |field|
-        fields << field unless fields.include?(field)
-      end
-    end
-    csv << fields
-    results.each do |result|
-      row = []
-      fields.each do |field|
-        row.push(result[field])
-      end
-      csv << row
-    end
-  end
-end
-
 def confluence_get_content(id)
   result = nil
   url = "#{API}/content/#{id}?expand=body.storage"
@@ -371,18 +301,26 @@ def get_all_links
   write_csv_file('links.csv', links)
 end
 
-def download_images
-
+def download_image(url, count, total)
+  filepath = "#{IMAGES}/#{SecureRandom.urlsafe_base64}.gif"
+  puts "#{count.to_s.rjust(total.to_s.length, ' ')}/#{total} #{(count*100/total).floor.to_s.rjust(3, ' ')}% #{url}"
+  begin
+    content = RestClient::Request.execute(method: :get, url: url)
+    IO.binwrite(filepath, content)
+  rescue RestClient::ExceptionWithResponse => e
+    rest_client_exception(e, 'GET', url)
+  end
 end
 
-def download_all_images
-#
-# links.each do |link|
-#   next unless link['tag'] == 'images'
-#   url = link['value']
-#   image = url.
-#     filepath = "#{IMAGES}/#{image}"
-# end
+def download_images
+  links = csv_to_array('links.csv')
+  images = links.filter { |link| link['tag'] == 'image' }
+  total = images.length
+  puts "\nDownloading #{total} images"
+  images.each_with_index do |image, index|
+    download_image(image['value'], index + 1, total)
+  end
+end
 
 # while File.exist?(filepath)
 #   nr += 1
@@ -411,7 +349,6 @@ def download_all_images
 # rescue RestClient::ExceptionWithResponse => e
 #   rest_client_exception(e, 'GET', url)
 # end
-end
 
 def get_links
   results = []
@@ -475,7 +412,8 @@ end
 
 def create_confluence_page(c)
   parent = get_parent(c[:offset])
-  puts "#{parent ? parent[:offset] + ' \'' + parent[:name] + '\' :: ' : ''}#{c[:offset]} #{c[:type]} '#{c[:name]}' #{c[:id]} #{c[:url]}"
+  p = parent ? "#{parent[:offset]} '#{parent[:name]} :: " : ''
+  puts "#{p}#{c[:offset]} #{c[:type]} '#{c[:name]}' #{c[:id]} #{c[:url]}"
 end
 
 def create_confluence_pages(categories_tree)
@@ -499,7 +437,7 @@ show_categories(@categories_tree)
 @offset_to_item = build_offset_to_item(@categories_tree, @offset_to_item)
 sanity_check
 # get_all_links
-download_all_images
+download_images
 create_confluence_pages(@categories_tree)
 
 puts "\nDone!"
